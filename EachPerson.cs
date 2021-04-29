fbf722b73aea649e2f02066a08c4353bb3403d56
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-29 07:37:23 +0530

    [Priyanshu]added address book to store multiple address book

---
+++ EachPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook2
+{
+    class EachPerson
+    {
+        public interface IPerson
+        {
+            // void addPerson();
+            void displayPerson();
+            void editPerson();
+            void deletePerson();
+        }
+    }
+}
